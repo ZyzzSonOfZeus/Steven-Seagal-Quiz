7174e76a432cad106debe10b01a506a5d95a44d7
Zyzz <111468764+ZyzzSonOfZeus@users.noreply.github.com>
2022-08-18 11:48:22 +1000

    Update 1.2

--- Final Steven Seagal Game.py
+++ Final Steven Seagal Game.py
@@ -1,47 +1,62 @@
-print("Welcome to Master-Sifu Steven Seagals Quiz!")
+def main():
 
-playing = input("Do you want to play? ")
+    print("Welcome to Master-Sifu Steven Seagals Quiz!")
 
-if playing.lower() != "yes":
-    quit()
+    playing = input("Do you want to play? ")
 
-print("Okay! Let's play :)")
-score = 0
+    if playing.lower() != "yes":
+        quit()
 
-answer = input("what martial art does Steven practice/teach? ")
-if answer.lower() == "aikido":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+    print("Okay! Let's play :)")
+    score = 0
 
-answer = input("What is Steven's favourite food? ")
-if answer.lower() == "hot wings":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+    answer = input("what martial art does Steven practice/teach? ")
+    if answer.lower() == "aikido":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
 
-answer = input("What brand is Steven Seagal's orange tinted glasses? ")
-if answer.lower() == "oakley":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+    answer = input("What is Steven's favourite food? ")
+    if answer.lower() == "hot wings":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
 
-answer = input("What type of hairline does Steven have? ")
-if answer.lower() == "receding":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+    answer = input("What brand is Steven Seagal's orange tinted glasses? ")
+    if answer.lower() == "oakley":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
 
-answer = input("How many wives has Steven gone through? ")
-if answer.lower() == "four":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+    answer = input("What type of hairline does Steven have? ")
+    if answer.lower() == "receding":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
 
-print("You got " + str(score) + " questions correct!")
-print("You got " + str((score / 5) * 100) + "%.")
+    answer = input("How many wives has Steven gone through? ")
+    if answer.lower() == "four":
+        print('Correct!')
+        score += 1
+    elif answer.lower() == "4":
+        print('Correct')
+        score += 1
+    else:
+        print("Incorrect!")
+
+    print("You got " + str(score) + " questions correct!")
+    print("You got " + str((score / 5) * 100) + "%")
+    
+    restart = input("Do you want to start again? ").lower()
+    if restart == "yes":
+        main()
+
+    else:
+        print("Have a nice day! :)")
+        exit()
+
+main()
